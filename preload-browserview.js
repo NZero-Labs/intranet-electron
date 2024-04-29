907e55cb0c08828d915ac6d9d9f66e3b5cebf492
Victor Evangelista <auxiliar.ti@amrabrasil.com.br>
2024-04-29 09:15:30 -0300

    fix: not formatting numbers correctly

--- preload-browserview.js
+++ preload-browserview.js
@@ -1,4 +1,71 @@
 const { ipcRenderer, clipboard } = require("electron");
+function validaCPF(cpf) {
+  let Soma = 0;
+  let Resto;
+
+  const strCPF = String(cpf);
+
+  if (strCPF.length !== 11) return false;
+
+  if (
+    [
+      "00000000000",
+      "11111111111",
+      "22222222222",
+      "33333333333",
+      "44444444444",
+      "55555555555",
+      "66666666666",
+      "77777777777",
+      "88888888888",
+      "99999999999",
+    ].indexOf(strCPF) !== -1
+  )
+    return false;
+
+  for (let i = 1; i <= 9; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+
+  Resto = (Soma * 10) % 11;
+
+  if (Resto == 10 || Resto == 11) Resto = 0;
+
+  if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+  Soma = 0;
+
+  for (let i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+
+  Resto = (Soma * 10) % 11;
+
+  if (Resto == 10 || Resto == 11) Resto = 0;
+
+  if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+
+  return true;
+}
+
+function validaCNPJ(cnpj) {
+  const b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  const c = String(cnpj);
+
+  if (c.length !== 14) return false;
+
+  if (/0{14}/.test(c)) return false;
+
+  let i = 0;
+  let n = 0;
+  for (i; i < 12; n += parseInt(c[i]) * b[++i]);
+  if (parseInt(c[12]) !== ((n %= 11) < 2 ? 0 : 11 - n)) return false;
+
+  i = 0;
+  n = 0;
+  for (i; i <= 12; n += parseInt(c[i]) * b[i++]);
+  if (parseInt(c[13]) !== ((n %= 11) < 2 ? 0 : 11 - n)) return false;
+
+  return true;
+}
 function getTableSelector(element) {
   // Percorre os elementos pai até encontrar a tabela mais próxima
   while (element && element.tagName !== "TABLE") {
@@ -44,11 +111,15 @@ function tryToConvertStringToNumber(stringToConvert) {
 
     return numero.toString().replace(".", ",");
   };
-  if (stringToConvert.startsWith("R$") || stringToConvert.endsWith("KWP")) {
-    return converter(stringToConvert);
-  } else {
-    // Retorna NaN se a string não estiver no formato de dinheiro
-    return stringToConvert;
+  switch (true) {
+    case stringToConvert.startsWith("R$") || stringToConvert.endsWith("KWP"):
+      return converter(stringToConvert);
+    case validaCPF(stringToConvert):
+      return `=TEXTO("${stringToConvert}";"00000000000")`;
+    case validaCNPJ(stringToConvert):
+      return `=TEXTO("${stringToConvert}";"00000000000000")`;
+    default:
+      return `"${stringToConvert}"`;
   }
 }
 function getSelectedText(table) {
@@ -71,24 +142,29 @@ function getCellText(cell) {
 
   // Função recursiva para percorrer os elementos dentro da célula
   const traverse = (element) => {
-    if (element.nodeType === 3) {
+    const tagName = element.tagName?.toLowerCase();
+    if (element.nodeType === 3 || tagName === "p") {
       // Se for um nó de texto
       cellText +=
         cellText === ""
-          ? `${tryToConvertStringToNumber(element.textContent.trim())}`
-          : `\t${tryToConvertStringToNumber(element.textContent.trim())}`; // Adiciona o texto do nó de texto
+          ? `${tryToConvertStringToNumber(element.textContent.trim() || " ")}`
+          : `\t${tryToConvertStringToNumber(
+              element.textContent.trim() || " "
+            )}`; // Adiciona o texto do nó de texto
     } else if (element.nodeType === 1) {
       // Se for um elemento HTML
-      const tagName = element.tagName.toLowerCase();
-      if (tagName === "div") {
-        cellText += " "; // Adiciona um espaço antes de elementos div
+      if (tagName === "div" || tagName === "p") {
+        cellText += "\t"; // Adiciona um espaço antes de elementos div
       }
       // Percorre os elementos filhos
       for (let i = 0; i < element.childNodes.length; i++) {
         traverse(element.childNodes[i]);
       }
-      if (tagName === "div") {
-        cellText += " "; // Adiciona um espaço após elementos div
+      if (element.childNodes.length === 0) {
+        cellText += " ";
+      }
+      if (tagName === "div" || tagName === "p") {
+        cellText += "\t"; // Adiciona um espaço após elementos div
       }
     }
   };
